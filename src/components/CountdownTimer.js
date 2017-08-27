fd0aaf1aedf2f79f03eb6188a71818aab34071d4
ChetanPate1 <=>
2017-08-27 17:15:46 +0100

    component style added, component structure made

---
+++ src/components/CountdownTimer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import CountdownTimer from './FrostGlass';
+
+class CountdownTimer extends Component {
+   render() {
+
+      return (
+         <div className="watchlist-card">
+
+         </div>
+      );
+   }
+}
+
+export default CountdownTimer;
