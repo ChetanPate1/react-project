b259a17e2d883b48c889e6887ed5749d547cc75f
ChetanPate1 <=>
2017-08-30 22:40:37 +0100

    tab-panel scss, and js structure made

---
+++ src/components/TabPanels.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class TabPanels extends Component {
+   constructor(props){
+      super(props);
+
+   }
+
+   render() {
+      return (
+         <div className="tab-panels" >
+
+         </div>
+      );
+   }
+}
+
+export default TabPanels;
